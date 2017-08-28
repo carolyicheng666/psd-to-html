04354dcc302bdee282500f777a7c396fd5fa4966
carolyicheng666 <790496165@qq.com>
2017-08-28 10:06:25 +0800

    uncsss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require("gulp");
-var cleanCSS = require("gulp-clean-css");
-var concat = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
+var cleanCSS = require("gulp-clean-css"); //压缩css
+var uncss = require('gulp-uncss'); //清理无用css
+var concat = require('gulp-concat'); //合并文件
+var imagemin = require('gulp-imagemin'); //压缩图片
 
 gulp.task('min-css', () =>
     gulp.src('./dist/css/*.css')
     .pipe(concat("index.min.css"))
+    .pipe(uncss({
+        html: ['index.html']
+    }))
     .pipe(cleanCSS())
     .pipe(gulp.dest('./build/css'))
 );
