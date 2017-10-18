055222d3930b7ea06bc84728e0838472cd14e426
carolyicheng666 <790496165@qq.com>
2017-10-18 15:38:14 +0800

    gulp-html-replace

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require("gulp");
-var cleanCSS = require("gulp-clean-css"); //压缩css
-var uncss = require('gulp-uncss'); //清理无用css
-var concat = require('gulp-concat'); //合并文件
-var imagemin = require('gulp-imagemin'); //压缩图片
+var cleanCSS = require("gulp-clean-css"); /*压缩css*/
+var uncss = require('gulp-uncss'); /*清理无用css*/
+var concat = require('gulp-concat'); /*合并文件*/
+var imagemin = require('gulp-imagemin'); /*压缩图片*/
 /*var cssnano = require('gulp-cssnano');*/
-var clean = require('gulp-clean');//清理文件
-var pngquant = require('imagemin-pngquant');//深度压缩png图片
-var compass = require('gulp-compass');//compass将sass编译成css
-var watch = require('gulp-watch');//热更新
+var clean = require('gulp-clean');/*清理文件*/
+var pngquant = require('imagemin-pngquant');/*深度压缩png图片*/
+var compass = require('gulp-compass');/*compass将sass编译成css*/
+var watch = require('gulp-watch');/*热更新*/
+var htmlreplace = require('gulp-html-replace');/*依赖加载文件替换*/
 
 gulp.task('compass', () =>
     gulp.src('./dist/sass/*.scss')
@@ -22,7 +23,7 @@ gulp.task('min-css', ['compass'], () =>
     gulp.src('./dist/css/*.css')
     .pipe(concat("index.min.css"))
     .pipe(uncss({
-        html: ['index.html']
+        html: ['./dist/index.html']
     }))
     .pipe(cleanCSS())
     .pipe(gulp.dest('./build/css'))
@@ -45,7 +46,15 @@ gulp.task('min-img', ['min-css'], () =>
     .pipe(gulp.dest('./build/images'))
 );
 
-gulp.task('default', ['compass', 'min-css', 'min-img']);
+gulp.task('html', ['min-img'], () =>
+  gulp.src('./dist/index.html')
+    .pipe(htmlreplace({
+        'css': './css/index.min.css'
+    }))
+    .pipe(gulp.dest('./build'))
+);
+
+gulp.task('default', ['compass', 'min-css', 'min-img', 'html']);
 
 gulp.task('watch', () =>
     gulp.watch('./dist/**/*', ['default'])
