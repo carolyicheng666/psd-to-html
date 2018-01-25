0ef08a0e5fd9be67931db1505e258072ca27dcfb
carolyicheng666 <790496165@qq.com>
2018-01-25 16:55:22 +0800

    delete prototype

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import ScrollReveal from './src/js/scrollreveal.min.js'
 import App from './src/index.vue'
 
-Vue.prototype.ScrollReveal = ScrollReveal;
+//Vue.prototype.$ScrollReveal = ScrollReveal;
 
 new Vue({
   el: '#app',
