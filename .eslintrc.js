157b4f690abc4c15f2b79656545653e9a509ea9b
carolyicheng666 <790496165@qq.com>
2018-03-20 11:12:44 +0800

    :sparkles: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "semi": ["error", "always"]
+    }
+};
